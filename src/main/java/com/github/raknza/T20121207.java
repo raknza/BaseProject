a4f306ea415b44bd78a2b8df37428afdfe511c9d
raknza <llssf258@gmail.com>
2017-12-07 19:54:52 +0800

    tttttttttttttttt

---
+++ src/main/java/com/github/raknza/T20121207.java
@@ -0,0 +1,12 @@
+package com.github.raknza;
+
+public class T20121207 {
+
+    public static void main(){
+        System.out.println(testmethod(5,3));
+    }
+
+    public static int testmethod(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/github/raknza/T20121207Test.java
@@ -0,0 +1,17 @@
+package com.github.raknza;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class T20121207Test {
+    @Test
+    public void main() throws Exception {
+    }
+
+    @Test
+    public void testmethod() throws Exception {
+
+    }
+
+}
